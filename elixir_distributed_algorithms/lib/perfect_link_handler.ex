b9a109d56ca1bdc047341806e1f18d516023d4fc
EndlessRecurrence <georgesut@yahoo.com>
2023-04-12 22:53:41 +0300

    Add partially done broadcasting scenario
    The broadcasting still doesn't work, because of an unexpected issue:
    Since the PerfectLinkHandler only handles client connections which come and go, its state is erased often. Therefore, the data that is stored in its state such as the process id structures and its own process id struct are erased. One idea would be to send this data to a PerfectLinkLayerMemory process that is specifically designed to store state. The reason why we have to do this is to keep the server asynchronous, we cannot really avoid using the PerfectLinkHandler as it is in this situation.
    To fix the issue, the PerfectLinkLayer needs to spawn a separate custom GenServer process, let's name it "PerfectLinkLayerMemory". This custom GenServer will store our state information and will send and receive data to/from the PerfectLinkHandler. When initiating a PerfectLinkHandler instance, the PID of the PerfectLinkLayerMemory will be passed to it, thus enabling interprocess communication between the PerfectLinkHandler and the PerfectLinkLayerMemory.

--- elixir_distributed_algorithms/lib/perfect_link_handler.ex
+++ elixir_distributed_algorithms/lib/perfect_link_handler.ex
@@ -25,7 +25,19 @@ defmodule DistributedAlgorithmsApp.PerfectLinkHandler do
   end
 
   @impl true
-  def handle_info({:tcp, socket, packet}, state) do
+  def handle_info({:tcp_closed, _socket}, state) do
+    Logger.info("Socket is closed")
+    {:stop, {:shutdown, "Socket is closed"}, state}
+  end
+
+  @impl true
+  def handle_info({:tcp_error, _socket, reason}, state) do
+    Logger.error("Tcp error: #{inspect(reason)}")
+    {:stop, {:shutdown, "Tcp error: #{inspect(reason)}"}, state}
+  end
+
+  @impl true
+  def handle_info({:tcp, _socket, packet}, state) do
     Logger.info("Received packet: #{inspect(packet)}")
     <<_::binary-size(@message_size_in_bytes), binary_message::binary>> = packet
     message = Protobuf.decode(binary_message, Proto.Message)
@@ -56,27 +68,66 @@ defmodule DistributedAlgorithmsApp.PerfectLinkHandler do
           process_id_struct: Enum.at(filtered_process_structs, 0),
           process_id_structs: broadcasted_process_id_structs_from_hub,
           system_id: message.systemId}
-        IO.inspect new_state
+        IO.inspect new_state, label: "New state after process initialization"
         {:noreply, new_state}
 
       :APP_BROADCAST ->
-        Logger.info("Hub orders process #{state.owner <> "-" <> Integer.to_string(state.process_index)} to broadcast value #{network_message.message.appBroadcast.value.v}.")
-        {:noreply, state}
+        from_abstraction_id = get_in(message, [Access.key!(:networkMessage), Access.key!(:message), Access.key!(:FromAbstractionId)])
+        process_id_struct = Map.get(state, :process_id_struct)
+        IO.inspect process_id_struct, label: "Process id struct from APP_BROADCAST case branch"
+        case from_abstraction_id do
+          "hub" ->
+            Logger.info("Hub orders process #{state.owner <> "-" <> Integer.to_string(state.process_index)} to broadcast value #{network_message.message.appBroadcast.value.v}.")
+            send_broadcast_value_to_other_processes(state.process_id_structs, message, state.process_id_struct)
+            send_broadcast_value_to_hub(message, process_id_struct)
+            {:noreply, state}
+          _ ->
+            send_broadcast_value_to_hub(message, process_id_struct)
+            {:noreply, state}
+        end
 
       true -> {:noreply, state}
     end
   end
 
-  @impl true
-  def handle_info({:tcp_closed, _socket}, state) do
-    Logger.info("Socket is closed")
-    {:stop, {:shutdown, "Socket is closed"}, state}
-  end
 
-  @impl true
-  def handle_info({:tcp_error, _socket, reason}, state) do
-    Logger.error("Tcp error: #{inspect(reason)}")
-    {:stop, {:shutdown, "Tcp error: #{inspect(reason)}"}, state}
+  def send_broadcast_value_to_other_processes([], _, _), do: :ok
+  def send_broadcast_value_to_other_processes([process | list_of_processes], message, this_process)
+    when process.rank != this_process.rank and process.owner == this_process.owner do
+    address_bytes = Regex.split(~r/\./, process.host)
+    address_as_tuple_of_integers = Enum.map(address_bytes, fn byte -> String.to_integer(byte) end) |> List.to_tuple()
+    options = [:binary, active: false, packet: :raw]
+    {_socket_connection_status, socket} = :gen_tcp.connect(address_as_tuple_of_integers, process.port, options)
+
+    updated_message = update_in(message, [Access.key!(:networkMessage), Access.key!(:message), Access.key!(:FromAbstractionId)], fn _ -> nil end)
+    encoded_broadcast_message = updated_message |> Protobuf.encode()
+
+    :gen_tcp.send(socket, <<0, 0, 0, byte_size(encoded_broadcast_message)>> <> encoded_broadcast_message)
+    Logger.info("Sent broadcast message from chosen process to process #{process.owner}-#{Integer.to_string(process.index)}.")
+    send_broadcast_value_to_other_processes(list_of_processes, message, this_process)
+  end
+  def send_broadcast_value_to_other_processes([_ | list_of_processes], message, this_process), do:
+    send_broadcast_value_to_other_processes(list_of_processes, message, this_process)
+
+  def send_broadcast_value_to_hub(message, this_process) do
+    address_bytes = Regex.split(~r/\./, message.networkMessage.senderHost)
+    address_as_tuple_of_integers = Enum.map(address_bytes, fn byte -> String.to_integer(byte) end) |> List.to_tuple()
+    options = [:binary, active: false, packet: :raw]
+    {_socket_connection_status, socket} = :gen_tcp.connect(address_as_tuple_of_integers, message.networkMessage.senderListeningPort, options)
+
+    value = message.networkMessage.message.appBroadcast.value
+    IO.inspect this_process, label: "SEND_BROADCAST_VALUE_TO_HUB -> this_process"
+    updated_message = message
+    |> update_in([Access.key!(:networkMessage), Access.key!(:message), Access.key!(:appBroadcast)], fn _ -> nil end)
+    |> update_in([Access.key!(:networkMessage), Access.key!(:message), Access.key!(:type)], fn _ -> :APP_VALUE end)
+    |> update_in([Access.key!(:networkMessage), Access.key!(:message), Access.key!(:appValue)], fn _ -> struct(Proto.AppValue, value: value) end)
+    |> update_in([Access.key!(:networkMessage), Access.key!(:senderHost)], fn _ -> this_process.host end)
+    |> update_in([Access.key!(:networkMessage), Access.key!(:senderListeningPort)], fn _ -> this_process.port end)
+
+    encoded_broadcast_message = updated_message |> Protobuf.encode()
+
+    :gen_tcp.send(socket, <<0, 0, 0, byte_size(encoded_broadcast_message)>> <> encoded_broadcast_message)
+    Logger.info("Sent broadcast message from #{this_process.owner}-#{Integer.to_string(this_process.index)} to hub.")
   end
 
   @spec generate_process_registration_message(
@@ -105,7 +156,7 @@ defmodule DistributedAlgorithmsApp.PerfectLinkHandler do
     address_bytes = Regex.split(~r/\./, hub_address)
     address_as_tuple_of_integers = Enum.map(address_bytes, fn byte -> String.to_integer(byte) end) |> List.to_tuple()
     options = [:binary, active: false, packet: :raw]
-    {socket_connection_status, socket} = :gen_tcp.connect(address_as_tuple_of_integers, hub_port, options)
+    {_socket_connection_status, socket} = :gen_tcp.connect(address_as_tuple_of_integers, hub_port, options)
 
     encoded_registration_message =
       generate_process_registration_message("127.0.0.1", port, nickname, process_index)
